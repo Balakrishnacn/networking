9fbf81cc3462827a4beaaedcdd679c023501e950
Balakrishnacn <balakrishna-reddy.c-n@capgemini.com>
2026-05-07 13:44:45 +0530

    corrected the vpc file

--- Infra/vpc.tf
+++ Infra/vpc.tf
@@ -11,7 +11,6 @@ resource "aws_vpc" "sandbox_vpc" {
 
 ############################################
 # Internet Gateway
-# (Required to make subnets public)
 ############################################
 resource "aws_internet_gateway" "sandbox_igw" {
   vpc_id = aws_vpc.sandbox_vpc.id
@@ -28,9 +27,6 @@ resource "aws_subnet" "src_subnet" {
   vpc_id                  = aws_vpc.sandbox_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
-
-  # This ensures EC2 launched here
-  # automatically gets a Public IP
   map_public_ip_on_launch = true
 
   tags = {
@@ -58,7 +54,6 @@ resource "aws_subnet" "dst_subnet" {
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.sandbox_vpc.id
 
-  # THIS is what makes a subnet "public"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.sandbox_igw.id
@@ -70,17 +65,13 @@ resource "aws_route_table" "public_rt" {
 }
 
 ############################################
-# Route Table Association
-# (This step is CRITICAL)
+# Route Table Associations
 ############################################
-
-# Associate source subnet with public route table
 resource "aws_route_table_association" "src_subnet_assoc" {
   subnet_id      = aws_subnet.src_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Associate destination subnet with public route table
 resource "aws_route_table_association" "dst_subnet_assoc" {
   subnet_id      = aws_subnet.dst_subnet.id
   route_table_id = aws_route_table.public_rt.id
